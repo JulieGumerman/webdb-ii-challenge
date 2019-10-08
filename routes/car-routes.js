a24646f8567c99701ea53eee0273c23be8c62ad3
Julie Gumerman <julie.gumerman@gmail.com>
2019-10-08 15:26:27 -0600

    with seeds, get by id, and update

--- routes/car-routes.js
+++ routes/car-routes.js
@@ -4,6 +4,8 @@ const router = express.Router();
 
 const db = require("../data/dbConfig");
 
+/********  from "/" ********/
+
 router.get("/", (req, res) => {
     db.select("*").from("cars")
         .then(cars => res.status(200).json(cars))
@@ -14,6 +16,21 @@ router.post("/", (req, res) => {
     db("cars").insert(req.body)
         .then(car => res.status(200).json(car))
         .catch(error => res.status(500).json({ error: "Your old car was fine. No new car for you!!!"}))
+})
+
+/******** from "/:id"********/
+
+router.get("/:id", (req, res) => {
+    db.select("*").from("cars").where("id", "=", req.params.id)
+        .first()
+        .then(car => res.status(200).json(car))
+        .catch(err => res.status(500).json({ error: "Nope. No dice."}))
+})
+
+router.put("/:id", (req, res) => {
+    db("cars").where({id: req.params.id}).update(req.body)
+        .then(update => res.status(200).json(req.body))
+        .catch(err => res.status(500).json({ error: "It was right the first time."}))
 })
 
 
